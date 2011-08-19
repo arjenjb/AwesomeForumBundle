7bce922ebeedfff2b92d1214e683afe2d3f02de7
Arjen Brouwer <arjenjb@gmail.com>
2011-08-19 18:39:48 +0200

    Generated entities

--- src/Awesome/Bundle/ForumBundle/Entity/Reply.php
+++ src/Awesome/Bundle/ForumBundle/Entity/Reply.php
@@ -36,4 +36,94 @@ class Reply
      * @ORM\JoinColumn(name="topic_id", referencedColumnName="id")
      */
     private $topic;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set message
+     *
+     * @param text $message
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * Get message
+     *
+     * @return text 
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * Set posterName
+     *
+     * @param string $posterName
+     */
+    public function setPosterName($posterName)
+    {
+        $this->posterName = $posterName;
+    }
+
+    /**
+     * Get posterName
+     *
+     * @return string 
+     */
+    public function getPosterName()
+    {
+        return $this->posterName;
+    }
+
+    /**
+     * Set datePosted
+     *
+     * @param datetime $datePosted
+     */
+    public function setDatePosted($datePosted)
+    {
+        $this->datePosted = $datePosted;
+    }
+
+    /**
+     * Get datePosted
+     *
+     * @return datetime 
+     */
+    public function getDatePosted()
+    {
+        return $this->datePosted;
+    }
+
+    /**
+     * Set topic
+     *
+     * @param Awesome\Bundle\ForumBundle\Entity\Topic $topic
+     */
+    public function setTopic(\Awesome\Bundle\ForumBundle\Entity\Topic $topic)
+    {
+        $this->topic = $topic;
+    }
+
+    /**
+     * Get topic
+     *
+     * @return Awesome\Bundle\ForumBundle\Entity\Topic 
+     */
+    public function getTopic()
+    {
+        return $this->topic;
+    }
 }
--- src/Awesome/Bundle/ForumBundle/Entity/Topic.php
+++ src/Awesome/Bundle/ForumBundle/Entity/Topic.php
@@ -41,4 +41,118 @@ class Topic
      * @ORM\OrderBy({"datePosted" = "DESC"})
      */
     private $replies;
+    public function __construct()
+    {
+        $this->replies = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+    
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set message
+     *
+     * @param string $message
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * Get message
+     *
+     * @return string 
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * Set posterName
+     *
+     * @param string $posterName
+     */
+    public function setPosterName($posterName)
+    {
+        $this->posterName = $posterName;
+    }
+
+    /**
+     * Get posterName
+     *
+     * @return string 
+     */
+    public function getPosterName()
+    {
+        return $this->posterName;
+    }
+
+    /**
+     * Set datePosted
+     *
+     * @param datetime $datePosted
+     */
+    public function setDatePosted($datePosted)
+    {
+        $this->datePosted = $datePosted;
+    }
+
+    /**
+     * Get datePosted
+     *
+     * @return datetime 
+     */
+    public function getDatePosted()
+    {
+        return $this->datePosted;
+    }
+
+    /**
+     * Add replies
+     *
+     * @param Awesome\Bundle\ForumBundle\Entity\Reply $replies
+     */
+    public function addReplies(\Awesome\Bundle\ForumBundle\Entity\Reply $replies)
+    {
+        $this->replies[] = $replies;
+    }
+
+    /**
+     * Get replies
+     *
+     * @return Doctrine\Common\Collections\Collection 
+     */
+    public function getReplies()
+    {
+        return $this->replies;
+    }
 }
